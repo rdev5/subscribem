b8347446ac37fa2f9079439d92324879228425ee
rdev5 <rdev5@borjawebs.com>
2013-05-21 11:01:00 -0700

    Added subdomain constraint. Error mismatch running spec.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
+require 'subscribem/constraints/subdomain_required'
+
 Subscribem::Engine.routes.draw do
   root :to => 'dashboard#index'
   get '/sign_up', :to => 'accounts#new', :as => :sign_up
   post '/accounts', :to => 'accounts#create', :as => :accounts
+
+  constraints(Subscribem::Constraints::SubdomainRequired) do
+    scope :module => 'account' do
+      root :to => 'dashboard#index'
+    end
+  end
 end
