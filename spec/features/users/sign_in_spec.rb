97e547c9a68ad8f434c4db8b34fdf32d2c3a8c36
rdev5 <rdev5@borjawebs.com>
2013-05-21 14:03:35 -0700

    Added subdomain authentication. Accounts sign_up_spec.rb broken.

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -8,12 +8,43 @@ feature 'User sign in' do
   within_account_subdomain do
     scenario "signs in as an account owner successfully" do
       visit subscribem.root_path
+
       page.current_url.should == sign_in_url
+
       fill_in "E-mail", :with => account.owner.email
       fill_in "Password", :with => "password"
       click_button "Sign In"
+
       page.should have_content("You are now signed in.")
       page.current_url.should == root_url
     end
+  end
+
+  it 'attempts sign in with an invalid password and fails' do
+    visit subscribem.root_url(:subdomain => account.subdomain)
+
+    page.current_url.should == sign_in_url
+    page.should have_content('Please sign in.')
+
+    fill_in 'E-mail', :with => account.owner.email
+    fill_in 'Password', :with => 'blah'
+    click_button 'Sign In'
+
+    page.should have_content('Invalid e-mail or password.')
+    page.current_url.should == sign_in_url
+  end
+
+  it 'attempts sign in with an invalid e-mail address and fails' do
+    visit subscribem.root_url(:subdomain => account.subdomain)
+
+    page.current_url.should == sign_in_url
+    page.should have_content('Please sign in.')
+
+    fill_in 'E-mail', :with => 'blah@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Sign In'
+
+    page.should have_content('Invalid e-mail or password.')
+    page.current_url.should == sign_in_url
   end
 end
