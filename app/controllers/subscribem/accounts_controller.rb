8039e16b8ee3e7d343d8e598d7f61489051b7097
rdev5 <rdev5@borjawebs.com>
2013-05-21 06:42:47 -0700

    Simple authentication using has_secure_password and warden

--- app/controllers/subscribem/accounts_controller.rb
+++ app/controllers/subscribem/accounts_controller.rb
@@ -5,10 +5,15 @@ module Subscribem
 
     def new
       @account = Subscribem::Account.new
+      @account.build_owner
     end
 
     def create
       account = Account.create(params[:account])
+      
+      env['warden'].set_user(account.owner.id, :scope => :user)
+      env['warden'].set_user(account.id, :scope => :account)
+
       flash[:success] = 'Your account has been successfully created.'
       redirect_to subscribem.root_url
     end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,8 +5,13 @@ feature 'Accounts' do
     visit subscribem.root_url
     click_link 'Account Sign Up'
     fill_in 'Name', :with => 'Test'
+    fill_in 'E-mail', :with => 'test@example.com'
+    password_field_id = 'account_owner_attributes_password'
+    fill_in password_field_id, :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
     click_button 'Create Account'
     success_message = 'Your account has been successfully created.'
     page.should have_content(success_message)
+    page.should have_content('Signed in as test@example.com')
   end
 end
